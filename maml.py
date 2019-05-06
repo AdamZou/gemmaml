ac6bbe8cf05afcbd6088a8d246e57a92ca038829
yayizou <yayizou@didiglobal.com>
2019-05-06 15:19:00 +0800

    Update maml.py

--- maml.py
+++ maml.py
@@ -242,20 +242,20 @@ class MAML:
                 # Define the weights /  weights stands for the model nueral_net!!!!!!!
                 # run models with array input to initialize models
                 #random.seed(7)
-                if FLAGS.datasource == 'sinusoid':
-                    self.weights = weights = self.construct_weights()
-                    weights((self.inputa_init[0]).astype('float32'))
-        
-                    weights_a = self.construct_weights()
-                    weights_a((self.inputa_init[0]).astype('float32'))
-                    weights_b = self.construct_weights()
-                    weights_b((self.inputa_init[0]).astype('float32'))
-                    weights_a_stop = self.construct_weights()
-                    weights_a_stop((self.inputa_init[0]).astype('float32'))
-                    weights_b_stop = self.construct_weights()
-                    weights_b_stop((self.inputa_init[0]).astype('float32'))
-                    weights_output = self.construct_weights()
-                    weights_output((self.inputa_init[0]).astype('float32'))
+                #if FLAGS.datasource == 'sinusoid':
+                self.weights = weights = self.construct_weights()
+                weights((self.inputa_init[0]).astype('float32'))
+    
+                weights_a = self.construct_weights()
+                weights_a((self.inputa_init[0]).astype('float32'))
+                weights_b = self.construct_weights()
+                weights_b((self.inputa_init[0]).astype('float32'))
+                weights_a_stop = self.construct_weights()
+                weights_a_stop((self.inputa_init[0]).astype('float32'))
+                weights_b_stop = self.construct_weights()
+                weights_b_stop((self.inputa_init[0]).astype('float32'))
+                weights_output = self.construct_weights()
+                weights_output((self.inputa_init[0]).astype('float32'))
 
 
 
@@ -268,7 +268,7 @@ class MAML:
                     weights_output((self.inputa[0]).astype('float32'))
                     self.weights_test = weights_test = self.construct_weights()
                     weights_test((self.inputa[0]).astype('float32'))
-                    '''
+                    
                 else:
                     self.weights = weights = self.construct_weights()
                     weights((self.inputa_init[0]).astype('float32'))
@@ -289,7 +289,7 @@ class MAML:
                     #weights_a(self.inputa[0])
                     #self.weights_b = weights_b = self.construct_weights()
                     #weights_b(self.inputa[0])
-
+                    '''
 
             # outputbs[i] and lossesb[i] is the output and loss after i+1 gradient updates
             lossesa, outputas, lossesb, outputbs = [], [], [], []
